5c923611711853ab312dddd9aa2f5410bc5930e0
Nikodem <upior126@gmail.com>
2023-04-20 10:43:35 +0200

    map function added

--- learnLambda.py
+++ learnLambda.py
@@ -7,19 +7,23 @@ def exponentiation(x):
 def sum(x,y):
     return x+y
 
-def reverse(text):
-    return text[-1]
+def cube(x):
+    return x**3
 
 """
 Lambda functions
 """
 lamb_exponentiation = lambda x: x*x
 lamb_sum = lambda x,y: x+y
+lamb_cube = lambda x: x**3
 
 """
 Prints
 """
 
-print("Normal: ", exponentiation(2), sum(5,2))
-print("Lambda: ", lamb_exponentiation(2), lamb_sum(5,2))
-print(reverse("siemanko"))
+print("Normal: ", exponentiation(2), sum(5,2), cube(2))
+print("Lambda: ", lamb_exponentiation(2), lamb_sum(5,2), lamb_cube(2))
+
+numbers = [1, 2, 3, 4, 5]
+print("Normal: ", list(map(cube,numbers)))
+print("Lambda: ",list(map(lambda x: x**3, numbers)))
