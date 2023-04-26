d39a0d1bd2a4ef19d9d24236309ab1f1e87b6218
Nikodem <upior126@gmail.com>
2023-04-26 21:56:42 +0200

    imports are fun!

--- objective.py
+++ objective.py
@@ -1,30 +1,33 @@
 
 #declaring main class
-class animal:
+class Animal:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
 #declaring child classess
-class dog(animal):
+class Dog(Animal):
     def voice(self):
         print("woof",end=" ")
 
-class cat(animal):
+class Cat(Animal):
     def voice(self):
         print("meow",end=" ")
 
-#asking user basic questions about animal
+"""
+asking user basic questions about animal
 animal_type = input("Is your animal cat or dog? ")
 animal_name = input("What is the name of your " + animal_type + "? ")
 animal_age = input("How old is "+ animal_name + "? ")
 
+
 #animals creation
 if animal_type == "cat":
-    pet = cat(animal_name,animal_age)
+    pet = Cat(animal_name,animal_age)
 elif animal_type == "dog":
-    pet = dog(animal_name, animal_age)
+    pet = Dog(animal_name, animal_age)
 
 #using dog or cat function
 pet.voice()
 print("-",pet.name,"said")
+"""
